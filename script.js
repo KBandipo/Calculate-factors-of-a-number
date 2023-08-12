e0b299f8556200b59146dcdb395432d8492d90a0
kennybandy <nekbandy.com>
2023-08-12 21:31:20 +0100

    Implement the arithmetic

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 "use strict";
 //A Program that shows allfactors of a number the user provides
+const numb = parseInt(prompt("Enter the mumber:"));
+for (let i = 1; i <= numb; i++) {
+  if (numb % i == 0) {
+    console.log(i);
+  }
+}
