608c21fc859aa6a78c844789742bbc470b0a45ef
kennybandy <nekbandy.com>
2023-08-12 21:32:25 +0100

    Finish

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 "use strict";
 //A Program that shows allfactors of a number the user provides
+//User input number
 const numb = parseInt(prompt("Enter the mumber:"));
+//loop the number from 1 to the input number
 for (let i = 1; i <= numb; i++) {
+  //check the factors
   if (numb % i == 0) {
     console.log(i);
   }
